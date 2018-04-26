389ae95cdbb5305934a0baa6c802f8cc0c703fc2
Jonas PC WIN <jonas.oesterle@gmx.de>
2018-04-26 17:08:22 +0200

    [01] 13: wrong approach for mapping over two arrays

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,21 +1,23 @@
 defmodule Cards do
-
   # implicit return : last used value inside method is returned
   # iex -S mix : start the interactive shell (write recompile for restart)
   def create_deck do
-    values = ["Ace","Two","Three","Four","Five"] # double quotes, single quotes is support, but not common
+    # double quotes, single quotes is support, but not common
+    values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-    for suit <- suits do
-      suit
+    for value <- values do
+      for suit <- suits do
+        "#{value} of #{suit}"
+      end
     end
   end
 
   def shuffle(deck) do
-      Enum.shuffle(deck)
+    Enum.shuffle(deck)
   end
 
   def contains?(deck, card) do
-      Enum.member?(deck,card)
+    Enum.member?(deck, card)
   end
 end
