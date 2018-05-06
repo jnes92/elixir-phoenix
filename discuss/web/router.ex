8d8886d3d3d76bc5f58b12e61f8c450717761bc5
Jonas Österle <jonas.oesterle@gmx.de>
2018-05-06 14:44:58 +0200

    [08] 65: Added TopicController Module.

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -17,7 +17,7 @@ defmodule Discuss.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    get "/topics/new", TopicsController, :new
+    get "/topics/new", TopicController, :new
   end
 
   # Other scopes may use custom stacks.
