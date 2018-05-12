ce02d283bb1a3d30dff69b32333a30707227f47f
Jonas Österle <jonas.oesterle@gmx.de>
2018-05-12 18:09:13 +0200

    [11] 96: Adding OAuth config

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+if File.exists?("config/dev.secret.exs") do
+  import_config "dev.secret.exs"
+end
---
+++ discuss/config/dev.secret.exs
@@ -0,0 +1,3 @@
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "ca4f3ed3780791367ea8",
+  client_secret: "5794a1c4a09268253bfdda40799634c0a0bb8045"
