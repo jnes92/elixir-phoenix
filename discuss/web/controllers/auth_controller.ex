91aad1e01d05950507e0dbe6b32cc57a128b0215
Jonas Österle <jonas.oesterle@gmx.de>
2018-05-13 13:55:43 +0200

    [11] 102: Creating new users

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -8,5 +8,16 @@ defmodule Discuss.AuthController do
     user_params = %{token: auth.credentials.token, email: auth.info.email, provider: auth.provider}
     changeset = User.changeset(%User{}, user_params)
 
+    insert_or_update_user(changeset)
   end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
+  end
+
 end
