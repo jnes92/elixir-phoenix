ef34f646b2ab989bdda0a7301487fb9f42851dd5
Jonas PC WIN <jonas.oesterle@gmx.de>
2018-05-16 18:21:50 +0200

    [12] 106: Assigning user to connection

--- discuss/web/controllers/plugs/set_user.ex
+++ discuss/web/controllers/plugs/set_user.ex
@@ -1,15 +1,20 @@
 defmodule Discuss.Plugs.SetUser do
-    import Plug.Conn
-    import Phoenix.Controller
+    import Plug.Conn #for using assign()
+    import Phoenix.Controller #for using get_session
 
     alias Discuss.Repo
     alias Discuss.User
-    alias Discuss.Router.Helpers
 
     def init(_params) do 
     end 
 
     def call(conn, _params) do
-         
+        user_id = get_session(conn, :user_id)
+        cond do
+            user = user_id && Repo.get(User, user_id) -> 
+                assign(conn, :user, user)
+            true -> # If no condition gets executed, handle failure: 
+                assign(conn, :user, nil)
+        end 
     end 
 end
