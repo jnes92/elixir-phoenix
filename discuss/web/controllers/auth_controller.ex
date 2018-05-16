eaa5a8901dec12fe1b94eb7e2c4ff4ade5813cb8
Jonas PC WIN <jonas.oesterle@gmx.de>
2018-05-16 18:55:46 +0200

    [12] 108: Added signout handler

--- discuss/web/controllers/auth_controller.ex
+++ discuss/web/controllers/auth_controller.ex
@@ -10,6 +10,12 @@ defmodule Discuss.AuthController do
     signin(conn, changeset)
   end
 
+  def signout(conn, _params) do 
+    conn
+    |> configure_session(drop: true)
+    |> redirect(to: topic_path(conn, :index))
+  end 
+
   defp insert_or_update_user(changeset) do
     case Repo.get_by(User, email: changeset.changes.email) do
       nil ->
@@ -31,7 +37,5 @@ defmodule Discuss.AuthController do
         |> put_flash(:error, reason)
         |> redirect(to: topic_path(conn, :index))
     end
-
   end
-
 end
