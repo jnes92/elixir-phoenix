f92297505d91b9581e815f7ba93dc6e77dc1ede6
Jonas Österle <jonas.oesterle@gmx.de>
2018-05-22 18:31:16 +0200

    [13] 121: Adding Comments model

--- discuss/web/models/topic.ex
+++ discuss/web/models/topic.ex
@@ -4,10 +4,11 @@ defmodule Discuss.Topic do
   schema "topics" do
       field :title, :string
       belongs_to :user, Discuss.User
+      has_many :comments, Discuss.Comment
   end
 
   def changeset(struct, params  \\ %{}) do
-      struct 
+      struct
       |> cast(params, [:title])
       |> validate_required([:title])
   end
